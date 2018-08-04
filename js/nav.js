df4aba44f2c5a92369f1374030475bce47dd1a4f
Radosław <radziowojak@gmail.com>
2018-08-04 11:43:24 +0200

    zrobiono skrypt dodawania tła nawigacji

--- js/nav.js
+++ js/nav.js
@@ -4,3 +4,20 @@ document.getElementById('outerburger').addEventListener('click',()=>
 	document.getElementById('burger').classList.toggle('menu');
 	document.getElementById('burger').classList.toggle('close');
 });
+window.addEventListener('load',()=>
+{
+	if(!document.getElementById('nav').classList.contains('nav-transparent'))
+	{
+		document.getElementById('nav').classList.add('nav-active');
+	}
+	window.addEventListener('scroll',()=>
+	{
+		if(document.getElementById('nav').classList.contains('nav-transparent'))
+		{
+			if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30)
+				document.getElementById('nav').classList.add('nav-active');
+			else
+				document.getElementById('nav').classList.remove('nav-active');
+		}
+	});
+});
