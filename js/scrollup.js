5ba744d65b1dd915d77d4245e422f544d9cc1045
Maciej Witkowski <d3lusion@post.cz>
2018-08-05 03:00:47 +0200

    Zastąpiono scrollup prostszym skryptem

--- js/scrollup.js
+++
@@ -1,4 +0,0 @@
-let intervalId=0;const $scrollButton=document.querySelector('.scroll');function scrollStep(){if(window.pageYOffset===0){clearInterval(intervalId)}
-window.scroll(0,window.pageYOffset-50)}
-function scrollToTop(){intervalId=setInterval(scrollStep,12.66)}
-$scrollButton.addEventListener('click',scrollToTop)
