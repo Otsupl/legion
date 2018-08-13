4f2f437094028f461549ead952fa7aeaa3b61bb8
Radosław <radziowojak@gmail.com>
2018-08-13 21:29:46 +0200

    przeniesienie skryptu pokazywania scrollbuttona do oddzielnego pliku

---
+++ js/scroll.js
@@ -0,0 +1,30 @@
+function scrolldown()
+{
+	window.scroll({ top: (document.getElementById("photo_container").scrollHeight-90), left: 0, behavior: 'smooth' });
+}
+function scrollup()
+{
+	window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+}
+function isIndex()
+{
+	return document.getElementById("photo_container") ? true : false;
+}
+window.addEventListener('scroll',()=>
+{
+	if (
+		isIndex() ? 
+		(document.body.scrollTop > document.getElementById("photo_container").scrollHeight-100 || document.documentElement.scrollTop > document.getElementById("photo_container").scrollHeight-100) : 
+		(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) 
+	)
+	{
+		document.getElementById("scrollup").style.visibility = "visible";
+		document.getElementById("scrollup").style.opacity = "1";
+	}	
+	else
+	{
+		document.getElementById("scrollup").style.visibility = "hidden";
+		document.getElementById("scrollup").style.opacity = "0";    
+	}
+
+});
