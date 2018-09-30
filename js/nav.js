30b6a75d80cab6e8cf1f8a53ed661a7df60d3cc4
Radosław <radziowojak@gmail.com>
2018-09-30 16:22:22 +0200

    naprawiono dodanie tła do nawigacji po odświerzeniu

--- js/nav.js
+++ js/nav.js
@@ -4,20 +4,25 @@ document.getElementById('outerburger').addEventListener('click',()=>
 	document.getElementById('burger').classList.toggle('menu');
 	document.getElementById('burger').classList.toggle('close');
 });
+function nav()
+{
+	if(document.getElementById('nav').classList.contains('nav-transparent'))
+	{
+		if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30)
+			document.getElementById('nav').classList.add('nav-active');
+		else
+			document.getElementById('nav').classList.remove('nav-active');
+	}
+}
 window.addEventListener('load',()=>
 {
 	if(!document.getElementById('nav').classList.contains('nav-transparent'))
 	{
 		document.getElementById('nav').classList.add('nav-active');
 	}
-	window.addEventListener('scroll',()=>
-	{
-		if(document.getElementById('nav').classList.contains('nav-transparent'))
-		{
-			if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30)
-				document.getElementById('nav').classList.add('nav-active');
-			else
-				document.getElementById('nav').classList.remove('nav-active');
-		}
-	});
+	nav();
+});
+window.addEventListener('scroll',()=>
+{
+	nav();
 });
